747432820653a34e8d8052cd60363e6270808581
connorfurby <cafurby@stu.naperville203.org>
2022-09-14 12:47:36 -0500

    9/14/22

--- Chapter 3 RepoStarterCode/ch3notesstartercode.java
+++ Chapter 3 RepoStarterCode/ch3notesstartercode.java
@@ -19,28 +19,43 @@ public static void main (String [] args)
 
         Scanner keyboard = new Scanner(System.in);
         int num;
+        String school; 
         
+/**
         System.out.println("Enter a number");
         num = keyboard.nextInt();
-        
-        if (num > 0)
-        {
-            if (num < 13) // this must resolve to a boolean
+        //
+       // if (num > 0)
+        //{   
+            
+            // extended if statements equal to OR
+            if (num < 10)
+            {
+                keyboard.nextLine();//clear buffer
+                System.out.println("What Grade School did you attend?");
+                school = keyboard.nextLine();
+                if (school.equals("May Whitney"))
+                    System.out.print("GO! Lions!!!");
+                else if (school.equals("Steeple Run"))
+                    System.out.print("Go! Super Stars!!!");
+            }
+            else if (num < 13) // this must resolve to a boolean
                     System.out.println("You go to JRH");
-                else if(num < 20)
-                    System.out.println("You go to HS");
-                else if (num < 55)
-                    System.out.println
-                    ("You are younger and most likely better looking than Mr. Hayes");
-                    System.out.println("Welcome to the World");
-        }
+            else if(num < 20)
+                System.out.println("You go to HS");
+            else if (num < 55)
+                System.out.println("You are younger and most likely better looking than Mr. Hayes");
+            // else if (num > 0)
+            System.out.println("Welcome to the World");
+        //}
+**/
     
-/*
+/**
     String word1 = "apple";
     String word2 = "cat";
     String word3 = "dog";
 
-    if(word2.compareTo(word1)<0)//;end a statement BAD BAD simicolen
+    if(word1.compareTo(word2)<0)//;end a statement BAD BAD simicolen
     {
         System.out.println(word2 +" is before " + word1  );
         System.out.println("and cats are evil" );
@@ -48,8 +63,9 @@ public static void main (String [] args)
     else
         System.out.println(word1 +" is before " + word2  );
 
-    if(word2.compareTo(word3)<0)
-        System.out.println(word2 +" is before " + word3  );
+    if(word3.compareTo(word2)<0)//;( semicolen
+        System.out.println(word3 +" is before " + word2  );
+**/
 /*
 
       int num = 5, num1 =10, num2 = 4;  // standard hours in a work week
@@ -64,33 +80,43 @@ public static void main (String [] args)
        num2 = Keyboard.nextInt();
 
       System.out.println ();
-/*
-
-       boolean a = true, b = false, c = false,d;
-        //System.out.println(a  );
-      // c =(a = true) || (b = true);
-      // System.out.println(a + "-" + b + "-" + c);
+*/
 
 
-    //   c = (a = false) || (b = true);
-    //   System.out.println(a + "-" + b+ "-" + c);
-
-//     c =(a = false) && (b = true);
- //    System.out.print(a + "-" + b+ "-" + c);
-//
-//     c = (a = true) && (b = false);
-//     System.out.print(a + "-" + b + "-" + c);
-
-//     d =!(a = true) || (b = true) && (c = true);
-//     System.out.print(a + "-" + b + "-" + c+ "-" + d);
+boolean a = true, b = false, c = false,d;
+//System.out.println(a  );
+//c =(a = true) || (b = true); // the || aka the or comparator short circuits on true
+//System.out.println(a  );
+//System.out.println(a + "-" + b + "-" + c);
 
 
-/*  */
+//c = (a = false) || (b = true);
+//System.out.println(a + "-" + b+ "-" + c);
 
-}// end of main
+//c =(a = false) && (b = true); // the && aka the and copmparator needs all to be true otherwise false
+//System.out.print(a + "-" + b+ "-" + c);
+
+//c = (a = true) && (b = false);
+//System.out.print(a + "-" + b + "-" + c);
+
+//d =!(a = true) || (b = true) && (c = true);
+//System.out.print(a + "-" + b + "-" + c+ "-" + d);
 
 
-}// end of class
+/**/
+double wallet = 1 * .01 + 2 + 0.5 + 3 + 0.10 + 4 + 0.25;
+if (wallet == 1.41)
+    System.out.println("Correct");
+else
+    System.out.println("Add much?");
+ 
+
+
+
+}//end of main
+
+
+}//end of class
 
 
 
