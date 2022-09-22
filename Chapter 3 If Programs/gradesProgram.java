2805ff0a4d450c5cbfe5c1a0856c45471c9f1e6e
connorfurby <cafurby@stu.naperville203.org>
2022-09-22 12:39:11 -0500

    9/22/2022

---
+++ Chapter 3 If Programs/gradesProgram.java
@@ -0,0 +1,21 @@
+/*
+ * Connor Furby
+ * 9/22/2022
+ * Grades Program
+ * Translates Letter Grade into a Number
+ */
+
+import java.util.Scanner;
+
+public class gradesProgram
+{
+    public static void main(string[] args)
+    {
+        scanner sc = new Scanner(System.in);
+        System.out.print("Input your letter grade: ");
+        String letter = new String(keyboard.nextLine());
+        
+        
+    }
+    }
+}
