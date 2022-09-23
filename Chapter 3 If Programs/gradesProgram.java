805db43c4568b5467eaa118fd5c59f20754f7501
connorfurby <cafurby@stu.naperville203.org>
2022-09-23 11:42:02 -0500

    grade finished

--- Chapter 3 If Programs/gradesProgram.java
+++ Chapter 3 If Programs/gradesProgram.java
@@ -9,13 +9,55 @@ import java.util.Scanner;
 
 public class gradesProgram
 {
-    public static void main(string[] args)
+    public static void main(String[] args)
     {
-        scanner sc = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
         System.out.print("Input your letter grade: ");
-        String letter = new String(keyboard.nextLine());
-        
-        
-    }
+        String letgrade = new String(keyboard.nextLine());
+        char letter, extra;
+        double numgrade = 0, valid = 1;
+        letter = letgrade.charAt(0);
+        switch(letter)
+        {
+            case 'A':
+                numgrade = 4.0;
+                break;
+            case 'B':
+                numgrade = 3.0;
+                break;
+            case 'C':
+                numgrade = 2.0;
+                break;
+            case 'D':
+                numgrade = 1.0;
+                break;
+            case 'F':
+                numgrade = 0;
+                break;
+            default:
+                System.out.print("Not a Valid Input");
+                valid = 0;
+        }
+            
+        if (letgrade.length() == 2 && valid != 0)
+        {
+            extra = letgrade.charAt(1);
+            switch(extra)
+            {
+                case '+':
+                    if (letter != 'A' && letter != 'F')
+                        numgrade += 0.03;
+                    break;
+                case '-':
+                    if (letter != 'F')
+                        numgrade -= 0.03;
+                    break;
+                default:
+                    System.out.print("Not a Valid Input");
+                    valid = 0;
+            }
+        }
+        if (valid == 1)
+            System.out.print(numgrade + " is your numeric grade");
     }
 }
