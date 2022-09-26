aea9afd7f6f94d6f8ac3cfec22ac0e311bf2ed0a
connorfurby <cafurby@stu.naperville203.org>
2022-09-26 12:20:34 -0500

    9/26/22

--- Chapter 3 If Programs/triangleProgram.java
+++ Chapter 3 If Programs/triangleProgram.java
@@ -1,33 +1,36 @@
+/*
+* Connor Furby
+* 9/22/2022
+* Triangle Program
+* Tests if triangle is scalene, right, isosceles, or equilateral, given angles or sides
+*/
 
-/**
- * Write a description of class triangleProgram here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+import java.util.Scanner;
+
 public class triangleProgram
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class triangleProgram
-     */
-    public triangleProgram()
+    public static void main(String[] args)
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Would you like to input sides or angles?\n");
+        System.out.print("(input 's' for sides or 'a' for angles): ");
+        String choice = new String(keyboard.nextLine());
+        if (choice.compareTo("s") == 0)
+        {
+            System.out.print("Input side lengths with a space following each one: ");
+            double side1, side2, side3;
+            side1 = keyboard.nextDouble();
+            side2 = keyboard.nextDouble();
+            side3 = keyboard.nextDouble();
+            if (((side1 + side2) < side3) || ((side2 + side3) < side1) || ((side1 + side3) < side2) || (side1 == 0) || (side2 == 0) || (side3 == 0))
+                System.out.print("This is not a valid triangle");
+            else if (side1 == side2 && side2 == side3)
+                System.out.print("This is an equilateral triangle");
+            else if (side1 == side2 || side2 == side3 || side1 == side3)
+                System.out.print("This is an isosceles triangle");
+            else
+                System.out.print("This is a scalene triangle");
+        }
+            
     }
 }
