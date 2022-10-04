36d7e79e36dc67c2bca23d8a641ef6ef8f060b23
connorfurby <111527458+connorfurby@users.noreply.github.com>
2022-10-03 21:26:38 -0500

    finished labs 1.1

--- Chapter 3 If Programs/taxesProgram.java
+++ Chapter 3 If Programs/taxesProgram.java
@@ -13,27 +13,25 @@ public class taxesProgram
     public static void main(String[] args)
     {
         Scanner keyboard = new Scanner(System.in);
-        double taxrate, income, taxdue;
-        System.out.print("What is your yearly income? ");
+        double tax, income;
+        System.out.println("What is your yearly income? ");
         income = keyboard.nextDouble();
-        // decides what the tax percentage will be based off yearly income
-        if (income > 500000)
-            taxrate = 0.06;
-        else if (income > 250000)
-            taxrate = 0.05;
-        else if (income > 100000)
-            taxrate = 0.04;
-        else if (income > 75000)
-            taxrate = 0.03;
-        else if (income > 50000)
-            taxrate = 0.02;
+        // decides what the tax will be based off yearly income
+        if (income <= 50000)
+            tax = income * 0.01;
+        else if (income <= 75000 && 50000 < income)
+            tax = 50000 * 0.01 + (income - 50000) * 0.02;
+        else if (income <= 100000 && 75000 < income)
+            tax = 50000 * 0.01 + 25000 * 0.02 + (income - 75000) * 0.03;
+        else if (income <= 250000 && 100000 < income)
+            tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + (income - 100000) * 0.04;
+        else if (income <= 500000 && 250000 < income)
+            tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + 150000 * 0.04 + (income - 250000) * 0.05;
         else
-            taxrate = 0.01;
+            tax = 50000 * 0.01 + 25000 * 0.02 + 25000 * 0.03 + 150000 * 0.04 + 250000 * 0.05 + (income - 500000) * 0.06;
         // creates a format that will print in 2 decimal places with $
         NumberFormat moneyFormat = NumberFormat.getCurrencyInstance();
-        // calculates taxes due
-        taxdue = income * taxrate;
         // prints taxes
-        System.out.print("Taxes Due in 1913: " + moneyFormat.format(taxdue));
+        System.out.println("Taxes Due in 1913: " + moneyFormat.format(tax));
     }
 }
