522e15d20e5126169925fadd29f82b89c5098f7e
connorfurby <cafurby@stu.naperville203.org>
2022-10-13 11:55:18 -0500

    10/12/22

--- Chapter3B Starter Code/e07/FindMinimumValue.java
+++ Chapter3B Starter Code/e07/FindMinimumValue.java
@@ -4,8 +4,18 @@ public class FindMinimumValue
 {
    public static void main(String[] args)
    {
-      Scanner in = new Scanner(System.in);
-
+      Scanner keyboard = new Scanner(System.in);
+      int min, loop = 1;
+      System.out.print("Enter in numbers; press 'Q' to stop");
+      String currLine = new String(keyboard.nextLine());
+      if (currLine.charAt(currLine.length() - 1) == 'Q')
+          loop = 0;
+      while (loop == 1)
+      {
+          newNum = keyboard.nextInt());
+          if (newNum < min)
+              min = newNum;
+      }
       System.out.println("The minimum value was: " + min);
    }
 }
