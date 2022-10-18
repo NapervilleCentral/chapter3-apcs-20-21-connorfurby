15ca167f7ae52b49503b993195f886f0d2c5e01e
connorfurby <cafurby@stu.naperville203.org>
2022-10-18 12:38:54 -0500

    10/18/22

--- Chapter 3B Labs/montyHall.java
+++ Chapter 3B Labs/montyHall.java
@@ -9,10 +9,30 @@ public class montyHall
 {
     public static void main()
     {
-        int prizedoor = (int)(Math.random());
-        Random playerchoice1 = new Random();
-        int d1, d2, d3;
-        
+        int s1s = 0, s2s = 0;
+        for (int i = 1; i <= 1000; i++) 
+        {
+            Random randomizer = new Random();
+            int prizedoor = randomizer.nextInt(3);
+            int playerchoice = randomizer.nextInt(3);
+            int opendoor = randomizer.nextInt(3);
+            int otherclosed  = randomizer.nextInt(3);
+            System.out.println(prizedoor);
+            System.out.println(playerchoice);
+            System.out.println(opendoor);
+            System.out.println(otherclosed);
+            while (opendoor == playerchoice || opendoor == prizedoor)
+                opendoor = randomizer.nextInt(3);
+            while (otherclosed == opendoor || otherclosed == playerchoice)
+                otherclosed = randomizer.nextInt(3);
+            
+            if (otherclosed == prizedoor)
+                s1s += s1s;
+            else
+                s2s += s2s;
+        }
+        System.out.println("Strategy 1 Success: " + s1s);
+        System.out.println("Strategy 2 Success: " + s2s);
     }
 }
 
