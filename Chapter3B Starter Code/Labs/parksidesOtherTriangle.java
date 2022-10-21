79cdd0db4c94d80367c9795c6dd07c801e229aae
connorfurby <cafurby@stu.naperville203.org>
2022-10-21 12:21:54 -0500

    10/21/22

--- Chapter3B Starter Code/Labs/parksidesOtherTriangle.java
+++ Chapter3B Starter Code/Labs/parksidesOtherTriangle.java
@@ -18,20 +18,42 @@ public class parksidesOtherTriangle
         
         //line one
         int cl = 1;
+        int startnum = seed;
         for (int ll = size; ll >= 0; ll--)
         {
             int cn = seed;
             int ca = 0;
+            int runone = 1;
             for (int i = 0; i < ll; i++)
             {
-                for (int templl = ll; 
+                // adds spaces ---
+                for (int tempcl = cl - 1; tempcl >= 1 && runone == 1; tempcl--)
+                {
+                    System.out.print("  ");
+                }
+                // prints numbers
+                if (runone == 1)
+                {
+                    cn = startnum;
+                    ca = cl - 1;
+                }
                 if (cn > 9)
-                    cn = cn - 9;
+                    cn = cn % 9;
+                if (cn == 0)
+                    cn = 9;
+                
+                runone = 0;
                 System.out.print(cn + " ");
                 ca = ca + 1;
                 cn = cn + ca;
             }
             System.out.print("\n");
+            startnum = startnum + (cl + 1);
+            if (startnum > 9)
+                startnum = startnum % 9;
+            if (startnum == 0)
+                startnum = 9;
+            cl = cl + 1;
         }
     }
 }
